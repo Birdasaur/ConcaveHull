7d3d48b874f8a7dfbae328f6ef7daf335d6ce3d0
Sean Phillips <sean.phillips@jhuapl.edu>
2023-08-31 07:15:03 -0400

    converted to maven project

---
+++ src/main/java/ConcaveHullMain.java
@@ -0,0 +1,19 @@
+package edu.jhuapl.trinity;
+
+import javafx.application.Application;
+
+/**
+ * This class exists because of the current state of JavaFX in Java11+.
+ * Please see https://stackoverflow.com/questions/52569724/javafx-11-create-a-jar-file-with-gradle/52571719#52571719
+ * for solution origin.
+ */
+public class ConcaveHullMain {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Entering Concave Hull main...");
+        Application.launch(App.class, args);
+    }
+}
